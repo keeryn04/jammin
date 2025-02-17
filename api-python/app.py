5b5f568aba5f1b642f81ec85ae3b7f788d1a1bcf
keeryn04 <keerynjohnson@gmail.com>
2025-02-17 11:42:02 -0700

    (Progress) More Backend

--- api-python/app.py
+++ api-python/app.py
@@ -4,6 +4,6 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)  #Allows frontend to access the backend
 
-@app.route("/api/hello", methods=["GET"])
+@app.route("/api/", methods=["GET"])
 def hello():
     return jsonify({"message": "Hello from Flask!"})
