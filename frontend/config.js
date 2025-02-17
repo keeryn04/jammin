4e5c5c80eb6dfc0ac34b2ec103b8f04729affbcf
keeryn04 <keerynjohnson@gmail.com>
2025-02-17 12:05:21 -0700

    Update config.js

--- frontend/config.js
+++ frontend/config.js
@@ -1,3 +1,3 @@
-const API_URL = process.env.REACT_APP_API_URL || "https://jammin.vercel.app/api";
+const API_URL = process.env.REACT_APP_API_URL || "https://jammin-app.vercel.app/api";
 
 export default API_URL;
