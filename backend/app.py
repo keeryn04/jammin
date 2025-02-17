ab6bfa4b69c18ab3c7488eab28a958d288255fe8
keeryn04 <keerynjohnson@gmail.com>
2025-02-17 11:07:20 -0700

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -3,7 +3,7 @@ from flask import Flask, jsonify
 
 app = Flask(__name__, static_folder='path_to_vite_dist', static_url_path='/')
 
-@app.route('/api')
+@app.route('/api/hello', methods=['GET'])
 def hello():
     return jsonify(message="Hello from Flask!")
 
