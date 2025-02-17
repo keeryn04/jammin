a0445149ac201f11ad940fa6e76d6e3288385a4b
keeryn04 <keerynjohnson@gmail.com>
2025-02-17 12:44:36 -0700

    (Feature) Backend Working

--- api/app.py
+++ api/app.py
@@ -2,7 +2,7 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-@app.route("/api", methods=["GET"])
+@app.route("/api/status", methods=["GET"])
 def home():
     return jsonify({"message": "API is working!"})
 
