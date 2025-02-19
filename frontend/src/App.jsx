d4c1d69c17fbafa3ba4aaa913f630f6215d9325e
Samiul Haque <mdsamiul.haque@ucalgary.ca>
2025-02-18 17:13:33 -0700

    Sorry for the push to main but the file setup should give you an idea of how additional pages, + test pages should be formatted
    The Folder containing the same-name jsx file called QuickNav is where you can throw in a button to quickly go to a new page(s) or function(s) you wanna test more visually so that you dont have to throw the button into the homepage when thats more grounded

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,32 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import Homepage from './components/HomePage/Homepage';
+import LandingPage from './components/LandingPage/LandingPage';
+import LoginPage from './components/Login_Signup/LoginPage';
+import About from './components/About/About';
 
+const isAuthenticated = true; //replace with authentication check later
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
-    <>
+    <BrowserRouter>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <Routes>
+          <Route path="/" element={isAuthenticated ? <Homepage /> : <Navigate to="/Login" />} /> 
+          {/*If logged in, go to homepage, else go to Landing page by default*/}
+
+          <Route path="/Home" element={<Homepage />} />
+          {/*This is where the magics gonna happen, will contain child page, Dashboard*/}
+          <Route path="/Welcome" element={<LandingPage />} />
+          {/*Simple directory page, Login, Sign-up, About*/}
+          <Route path="/Login" element={<LoginPage />}/>
+          <Route path="/About" element={<About />}/>
+          <Route path="*" element={<Navigate to="/" />} />
+        </Routes>
       </div>
-      <h1 className='text-3xl font-bold underline'>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    </BrowserRouter>
   )
 }
 
