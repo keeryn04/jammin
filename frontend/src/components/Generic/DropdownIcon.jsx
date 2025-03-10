e1458a16f7474471c7dc2fc06ac3f9cb6a97dffd
Evan Mann <ejmann900@gmail.com>
2025-03-09 22:01:39 -0600

    (MAJOR FIX) removed a builder.io api key getting an svg file, instead downloaded the svg

--- frontend/src/components/Generic/DropdownIcon.jsx
+++ frontend/src/components/Generic/DropdownIcon.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const DropdownIcon = () => {
   return (
     <img
-      src="https://cdn.builder.io/api/v1/image/assets/TEMP/b647ce7e5d4d8fdcda5dda77ec84e961679d09ca741252b998fae4dded06b235?placeholderIfAbsent=true&apiKey=c01677bb73c24e638ae9f9818c2c8779"
+      src=".\downDownArrow.svg"
       className="object-contain absolute z-0 shrink-0 self-start aspect-square bottom-[18px] fill-neutral-400 h-[23px] right-[9px] w-[23px]"
       alt="Dropdown arrow"
       aria-hidden="true"
