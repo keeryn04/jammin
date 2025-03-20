c5170d4c6cb69f3f81ad26d872429013e95d006b
Evan Mann <ejmann900@gmail.com>
2025-03-20 11:18:50 -0600

    (FIX) removed nav bar bottom margin to potentially remove whitesapce

--- frontend/src/components/Dashboard/Sidebar.jsx
+++ frontend/src/components/Dashboard/Sidebar.jsx
@@ -4,7 +4,7 @@ import Navbar from "./Navbar";
 export default function Sidebar() {
   return (  
   <div className="flex flex-col items-center">
-    <img className="hidden sm:block w-[115px] h-[115px] m-4 mb-10" src="/icons/FlattenedLogo.svg" />
+    <img className="hidden sm:block w-[115px] h-[115px] m-4" src="/icons/FlattenedLogo.svg" />
     <Navbar />
   </div>
   );
