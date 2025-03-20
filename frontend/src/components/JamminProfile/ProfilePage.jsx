1e58d9be3a53f3aec98084e83456c70e122aec73
Evan Mann <ejmann900@gmail.com>
2025-03-20 12:18:24 -0600

    removed scroll bar from full jammin profile page to keep navbar at bottom

--- frontend/src/components/JamminProfile/ProfilePage.jsx
+++ frontend/src/components/JamminProfile/ProfilePage.jsx
@@ -21,14 +21,7 @@ const ProfilePage = () => {
   setTimeout(setAppHeight, 50); // Small delay to allow UI adjustments
 
   return (
-    <main className="flex flex-col sm:flex-row w-screen bg-neutral-800 h-[var(--app-height)]
-      overflow-y-auto [&::-webkit-scrollbar]:w-2
-      [&::-webkit-scrollbar-track]:rounded-full
-      [&::-webkit-scrollbar-track]:bg-gray-100
-      [&::-webkit-scrollbar-thumb]:rounded-full
-      [&::-webkit-scrollbar-thumb]:bg-gray-300
-      dark:[&::-webkit-scrollbar-track]:bg-neutral-700
-      dark:[&::-webkit-scrollbar-thumb]:bg-neutral-500">
+    <main className="flex flex-col sm:flex-row w-screen bg-neutral-800 h-[var(--app-height)]">
       <Sidebar />
       <ProfileContent activeUser={activeUser} /> {/* Pass activeUser as a prop */}
     </main>
