6bc83103fe4415c2987ed6d8b9ee152f17d95a31
Evan Mann <ejmann900@gmail.com>
2025-03-20 10:29:39 -0600

    (MOBILE UPDATE) about changes

--- frontend/src/components/About Page/AboutContainer.jsx
+++ frontend/src/components/About Page/AboutContainer.jsx
@@ -6,7 +6,14 @@ import Sidebar from "../Dashboard/Sidebar";
 
 const AboutContainer = () => {
   return (
-    <main className="flex flex-col sm:flex-row w-screen bg-neutral-800 h-screen ">
+    <main className="flex flex-col sm:flex-row w-screen bg-neutral-800 h-screen
+      overflow-y-auto [&::-webkit-scrollbar]:w-2
+      [&::-webkit-scrollbar-track]:rounded-full
+      [&::-webkit-scrollbar-track]:bg-gray-100
+      [&::-webkit-scrollbar-thumb]:rounded-full
+      [&::-webkit-scrollbar-thumb]:bg-gray-300
+      dark:[&::-webkit-scrollbar-track]:bg-neutral-700
+      dark:[&::-webkit-scrollbar-thumb]:bg-neutral-500">
       <Sidebar/>  
       <div className="flex flex-1 flex-col items-center sm:order-none order-first">
 
@@ -23,9 +30,9 @@ const AboutContainer = () => {
         </div>
 
         <div className="flex flex-col w-[134px] sm:w-[100%] h-full items-center justify-center">
-          <h1 className="text-center text-2xl font-bold text-white px-40 mb-5">Jammin is a project created for SENG 401 - Software Architecture that uses ChatGPT to parse a user’s Spotify analytics and match them with people with similar music taste. Matches are made based on both user’s top artists, songs and genres, combining to generate a compatibility score between both parties.</h1>
+          <h1 className="text-center text-md sm:test-2xl font-bold text-white px-40 mb-5">Jammin is a project created for SENG 401 - Software Architecture that uses ChatGPT to parse a user’s Spotify analytics and match them with people with similar music taste. Matches are made based on both user’s top artists, songs and genres, combining to generate a compatibility score between both parties.</h1>
           <br></br>
-          <h2 className="text-center text-xl font-bold text-white">Credits: Samiul Haque <br/> Keeryn Johnson <br/> Elias Poitras-Whitecalf <br/> Petr Dubovsky <br/> Ryan Graham <br/> Evan Mann</h2>
+          <h2 className="text-center text-sm sm:test-xl font-bold text-white">Credits: Samiul Haque <br/> Keeryn Johnson <br/> Elias Poitras-Whitecalf <br/> Petr Dubovsky <br/> Ryan Graham <br/> Evan Mann</h2>
         </div>
         
       </div>
