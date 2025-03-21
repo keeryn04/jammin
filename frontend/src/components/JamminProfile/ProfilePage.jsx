7dbf8df7923dcc10006ea03cc8d3f0b4fcec5a27
Samiul Haque <mdsamiul.haque@ucalgary.ca>
2025-03-21 09:57:40 -0600

    Revert "(UPDATE) added profile header to Jammin profile"
    This reverts commit 55fb17bcb116e05b66ed33853997348e5edd4595.

--- frontend/src/components/JamminProfile/ProfilePage.jsx
+++ frontend/src/components/JamminProfile/ProfilePage.jsx
@@ -23,18 +23,6 @@ const ProfilePage = () => {
   return (
     <main className="flex flex-col sm:flex-row w-screen bg-neutral-800 h-[var(--app-height)]">
       <Sidebar />
-
-      <div className="w-[100%]">
-          <h1 className="   m-5 text-6xl
-                            font-bold text-left 
-                            text-white max-w-[866px] 
-                            max-md:max-w-[700px] 
-                            max-sm:max-w-full 
-                            ">
-            Profile
-          </h1>
-        </div>
-        
       <ProfileContent activeUser={activeUser} /> {/* Pass activeUser as a prop */}
     </main>
   );
