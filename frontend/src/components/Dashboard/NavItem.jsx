059b559ee04a4d8c12ec25c5baa59b0895e320a0
Evan Mann <ejmann900@gmail.com>
2025-03-21 13:15:01 -0600

    (FIX) updated the size of the logout icon, fixed a spelling mistake

--- frontend/src/components/Dashboard/NavItem.jsx
+++ frontend/src/components/Dashboard/NavItem.jsx
@@ -11,7 +11,7 @@ export function NavItem({ icon, clickFunction}) {
 
 export function LogOutNavItem({ logout_icon, red_logout_icon, clickFunction }) { 
     return (
-        <button onClick = {clickFunction} className="group flex justify-center items-center cursor-pointer sm:h-[80%] sm:w-[80%] h-[20%] w-[20%]">
+        <button onClick = {clickFunction} className="group flex justify-center items-center cursor-pointer sm:h-[80%] sm:w-[80%] h-[8%] w-[8%]">
             <img className="group-hover:hidden" src={`${logout_icon}`} />
             <img className="hidden group-hover:block" src={`${red_logout_icon}`} />
         </button>
