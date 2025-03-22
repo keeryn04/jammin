7a4a7235c89d90afa4a340d35052789a281b5b1e
Samiul Haque <mdsamiul.haque@ucalgary.ca>
2025-03-21 18:34:51 -0600

    (UPDATE) genres responsively displayed

--- frontend/src/components/Profile/GenreSection.jsx
+++ frontend/src/components/Profile/GenreSection.jsx
@@ -4,30 +4,24 @@ import { UserContext } from "../UserContext";
 
 const GenreSection = () => {
   const [genres, setGenres] = useState([]);
-    // Access context values
-    const {
-      currentDisplayedUser,
-    } = useContext(UserContext);
+  // Access context values
+  const { currentDisplayedUser } = useContext(UserContext);
 
-    useEffect(() => {
-      const topGenres = currentDisplayedUser.top_genres ? currentDisplayedUser.top_genres.split(", ") : [];
-      setGenres(topGenres);
-    }, [currentDisplayedUser]);
+  useEffect(() => {
+    const topGenres = currentDisplayedUser.top_genres
+      ? currentDisplayedUser.top_genres.split(", ")
+      : [];
+    setGenres(topGenres);
+  }, [currentDisplayedUser]);
 
   return (
     <section className="px-0 py-4 w-[400px] max-md:w-full">
       <h2 className="mb-4 text-base leading-6 text-center text-white">
         Top Genres
       </h2>
-      <div className="flex flex-col gap-3 p-4">
-        {[0, 1, 2].map((rowIndex) => (
-          <div
-            key={rowIndex}
-            className="flex gap-3 justify-between max-sm:flex-col"
-          >
-            <GenreCard genre={genres[rowIndex * 2]} />
-            <GenreCard genre={genres[rowIndex * 2 + 1]} />
-          </div>
+      <div className="grid grid-cols-2 gap-3 p-4">
+        {genres.slice(0, 6).map((genre, index) => (
+          <GenreCard key={index} genre={genre} />
         ))}
       </div>
     </section>
